1213c8b0179bd3f6e0587d0f67404e29613fc986
ahmad sultan <ahmad.soltan097@gmail.com>
2022-04-16 05:29:08 +0300

    last update

--- index.ts
+++ index.ts
@@ -8,6 +8,7 @@ const loadKeyPairs = (path: string) => {
     const content = fs.readFileSync(path, { encoding: 'utf8' })
     const secretKey = Uint8Array.from(JSON.parse(content))
     keyPairs = Keypair.fromSecretKey(secretKey);
+    console.log(keyPairs.publicKey.toString())
 }
 
 const establishConnection = async (rpcUrl: string) => {
@@ -35,11 +36,13 @@ const createAccount = async (seed: string, space: number, connection: Connection
     })
 
     const transaction = new Transaction().add(instruction)
-
-    const result = await sendAndConfirmTransaction(connection, transaction, [keyPairs])
+    const hash = await connection.sendTransaction(transaction,[keyPairs])
+    console.log(hash);
+    const result = await connection.confirmTransaction(hash);
     console.log(result)
 }
 loadKeyPairs('./keys.json')
-establishConnection("https://8899-ahmadlpsn-crowdfundingpl-h8n7fc3l7t3.ws-eu39a.gitpod.io/")
-    .then(connection => createAccount("campaign 2", 1024, connection))
-    .catch(console.log)
+establishConnection("https://8899-ahmadlpsn-crowdfundingpl-h8n7fc3l7t3.ws-eu40.gitpod.io/")
+
+    .then(connection => createAccount("campaign 7", 1024, connection))
+    .catch(console.log) 
